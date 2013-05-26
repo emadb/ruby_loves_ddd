a8e4e7e93c01994034e73283bf36e431648efc7f
Emanuele DelBono <emanuele@codiceplastico.com>
2013-05-26 22:55:16 +0200

    removed mongo.

--- lib/basket/basket_repository.rb
+++ lib/basket/basket_repository.rb
@@ -1,25 +1,11 @@
-require 'mongo'
-
 class BasketRepository
-  include Mongo
-
-  def initialize
-    db = MongoClient.new('localhost', 27017).db('eshop')
-    @baskets_collection = db["baskets"]
-  end
-
+   
+ 
   def save (basket)
-    if basket.id.nil?
-      hash = {}
-      basket.instance_variables.each {|var| hash[var.to_s.delete("@")] = basket.instance_variable_get(var) }
-
-      @baskets_collection.insert hash
-    else
-      @baskets_collection.update({"_id" => id}, basket)
-    end
+    # save the aggregate to the database
   end
 
   def get (basket_id)
-    @baskets_collection.find_one("_id" => id)
+    #get the aggregate from database
   end
 end
