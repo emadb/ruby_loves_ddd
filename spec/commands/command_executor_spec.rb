b48cca77515919817bbb1d5d9ded21de038c9a44
Emanuele DelBono <emanuele@codiceplastico.com>
2013-05-30 22:29:55 +0200

    removed spaces

--- spec/commands/command_executor_spec.rb
+++ spec/commands/command_executor_spec.rb
@@ -11,22 +11,21 @@ describe CommandExecutor do
     execute_command command
     expect(command.is_called).to be_true
   end
-  
 end
 
 
-  module FakeCommands
-    class FakeTestCommand
-      attr_accessor :is_called
-      def initialize(name)
-      end
+module FakeCommands
+  class FakeTestCommand
+    attr_accessor :is_called
+    def initialize(name)
     end
   end
+end
 
-  module Handlers
-    class FakeTestHandler
-      def execute (command)
-        command.is_called = true
-      end
+module Handlers
+  class FakeTestHandler
+    def execute (command)
+      command.is_called = true
     end
   end
+end
