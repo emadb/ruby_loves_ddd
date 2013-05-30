372f88ec30b51fb635de6c2804878694f0a3a676
Emanuele DelBono <emanuele@codiceplastico.com>
2013-05-30 17:49:15 +0200

    refactoring folders

--- spec/command_executor_spec.rb
+++ spec/command_executor_spec.rb
@@ -11,21 +11,22 @@ describe CommandExecutor do
     execute_command command
     expect(command.is_called).to be_true
   end
+  
 end
 
 
-module FakeCommands
-  class FakeTestCommand
-    attr_accessor :is_called
-    def initialize(name)
+  module FakeCommands
+    class FakeTestCommand
+      attr_accessor :is_called
+      def initialize(name)
+      end
     end
   end
-end
 
-module Handlers
-  class FakeTestHandler
-    def execute (command)
-      command.is_called = true
+  module Handlers
+    class FakeTestHandler
+      def execute (command)
+        command.is_called = true
+      end
     end
   end
-end
