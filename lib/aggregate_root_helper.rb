a7f6515b833dd868c6d21903cfa9eeb1fe0eb219
Emanuele DelBono <emanuele@codiceplastico.com>
2013-06-07 17:30:44 +0200

    resolved conflict

--- lib/aggregate_root_helper.rb
+++ lib/aggregate_root_helper.rb
@@ -1,8 +1,27 @@
 module AggregateRootHelper
-	def raise_event event, *args
-    hash = get_hash event
-    hash[:subscribers].each do |s|
-      s[:instance].send(s[:method])
+  @@subscribers = Hash.new{|hash,key| hash[key]=[]}
+
+  def id= id
+    @id = id
+  end
+
+  def id
+    @id
+  end
+
+  def self.included(klass)
+    klass.extend AggregateRootHelper
+  end
+  
+  def subscribe_to(event, method)
+    @@subscribers[event] << {klass: self, method: method}
+  end
+
+  def raise_event(event, *args)
+    @@subscribers[event].each do |subscriber| 
+      ObjectSpace.each_object subscriber[:klass] do |instance|
+        instance.send subscriber[:method], args
+      end
     end
 	end
 
