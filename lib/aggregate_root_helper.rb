7c8574d3eb82f9d8d3243590424d3078378b97f4
Emanuele DelBono <emanuele@codiceplastico.com>
2013-06-11 00:07:44 +0200

    refactoring through a real ES system

--- lib/aggregate_root_helper.rb
+++ lib/aggregate_root_helper.rb
@@ -22,21 +22,29 @@ module AggregateRootHelper
   end
 
   def raise_event(event, *args)
-     uncommited_events << {event: event, args: args}
+    uncommited_events << {name: event, args: args}
+    send "on_#{event}", *args
   end
 
   # TODO: error checking
   def commit
     while event = uncommited_events.shift
       send_event event
+      # store event : {:aggregate_id => id, :event => event}
     end
   end
 
-  def send_event send_event
-    @@subscribers[event].each do |subscriber| 
+  def send_event event
+    @@subscribers[event.name].each do |subscriber| 
       ObjectSpace.each_object subscriber[:klass] do |instance|
-        instance.send subscriber[:method], args
+        instance.send subscriber[:method], event.args
       end
+    end
+  end
+
+  def apply_events events
+    events.each do |e|
+      send "on_#{e[:name]}", e[:args]
     end
   end
 end
